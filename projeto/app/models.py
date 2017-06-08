be04c2c37bc586d2f4fba6e03f1fc6c162fb027f
Carlos Henrique <krloss@github.com>
2017-06-08 05:24:40 -0300

    modelo

--- projeto/app/models.py
+++ projeto/app/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Questao(models.Model):
+	texto = models.CharField(max_length=200)
+	data = models.DateTimeField('date published')
+
+class Escolha(models.Model):
+	questao = models.ForeignKey(Questao, on_delete=models.CASCADE)
+	texto = models.CharField(max_length=100)
+	voto = models.IntegerField(default=0)
