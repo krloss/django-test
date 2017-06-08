125d6aa1af41d0dd99a89cb2d8f674dfc9381d20
Carlos Henrique <krloss@github.com>
2017-06-08 05:56:42 -0300

    ajuste modelo

--- projeto/app/models.py
+++ projeto/app/models.py
@@ -2,13 +2,25 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
+
+import datetime
 
 # Create your models here.
 class Questao(models.Model):
 	texto = models.CharField(max_length=200)
 	data = models.DateTimeField('date published')
 
+	def __str__(self):
+		return self.texto
+
+	def recente(self):
+		return self.data > timezone.now() - datetime.timedelta(days=1)
+
 class Escolha(models.Model):
 	questao = models.ForeignKey(Questao, on_delete=models.CASCADE)
 	texto = models.CharField(max_length=100)
 	voto = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.texto
