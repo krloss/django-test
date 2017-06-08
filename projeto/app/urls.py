d3a69be9034676e59ac8547a5b64fa6f4514f87d
Carlos Henrique <krloss@github.com>
2017-06-08 08:25:49 -0300

    visoes genericas

--- projeto/app/urls.py
+++ projeto/app/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	# Modelo
-	url(r'^list/$',views.lista, name='questoes'),
-	url(r'^(?P<questao_id>[0-9]+)/$',views.detalhe, name='detalhe'),
-	url(r'^(?P<questao_id>[0-9]+)/results/$',views.resultados, name='resultados'),
-	url(r'^(?P<questao_id>[0-9]+)/vote/$',views.voto, name='voto'),
+	url(r'^list/$', views.lista, name='questoes'),
+	url(r'^questoes/$', views.QuestoesView.as_view(), name='listaQuestoes'),
+	url(r'^(?P<questao_id>[0-9]+)/$', views.detalhe, name='detalhe'),
+	url(r'^(?P<pk>[0-9]+)/detail/$', views.DetailView.as_view(), name='visaoDetalhe'),
+	url(r'^(?P<questao_id>[0-9]+)/results/$', views.resultados, name='resultados'),
+	url(r'^(?P<questao_id>[0-9]+)/vote/$', views.voto, name='voto'),
 ]
--- projeto/app/views.py
+++ projeto/app/views.py
@@ -4,12 +4,12 @@ from __future__ import unicode_literals
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from django.template import loader
 
 from .models import Questao,Escolha
 
 # Create your views here.
-
 def index(request):
 	return HttpResponse('Olá Mundo!!!')
 
@@ -20,6 +20,13 @@ def lista(request):
 	#return HttpResponse(';'.join([q.texto for q in questoes]))
 	return HttpResponse(template.render(context,request))
 
+class QuestoesView(generic.ListView):
+	template_name = 'apps/lista.html'
+	context_object_name = 'questoes'
+
+	def get_queryset(self):
+		return Questao.objects.order_by('-data')[:5]
+
 def detalhe(request,questao_id):
 	#try:
 	#	q = Questao.objects.get(pk=questao_id)
@@ -28,6 +35,10 @@ def detalhe(request,questao_id):
 	q = get_object_or_404(Questao, pk=questao_id)
 	#return HttpResponse("Questao %s." % questao_id)
 	return render(request,'apps/detail.html',{'questao':q})
+
+class DetailView(generic.DetailView):
+	model = Questao
+	template_name = 'apps/detail.html'
 
 def resultados(request,questao_id):
 	q = get_object_or_404(Questao, pk=questao_id)
